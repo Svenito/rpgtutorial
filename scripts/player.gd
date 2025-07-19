a6d8d9e6cec1b049341e2a3680ecc56beb606e70
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-19 17:22:15 +0100

    state machine changes and animation improvement
    Minor refactoring which introduces a lot more if statements. Will
    probably need to refactor a state transition method (set_state)
    
    or look directly into node based statemachine which seems to the godot
    way for easier debugging and visualisation

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,35 +23,60 @@ const IDLE_ANIMATIONS = {
 	Vector2.RIGHT: {"anim": "idle_side"},
 	Vector2.ZERO: {"anim": "idle_side"}
 	}
+	
+const ATTACK_ANIMATIONS = {
+	Vector2.UP: {"anim": "attack_back"},
+	Vector2.DOWN: {"anim": "attack_front"},
+	Vector2.LEFT: {"anim": "attack_side", "flip": true},  
+	Vector2.RIGHT: {"anim": "attack_side"},
+	Vector2.ZERO: {"anim": "attack_side"}
+	}
 
 enum States {IDLE, MOVING, ATTACKING, DYING}
 var state: States = States.IDLE
+var previous_state = state
 
 func _physics_process(delta: float) -> void:
-	player_movement(delta)
+	if state != States.ATTACKING:
+		move_and_slide()
+
+func _process(delta: float) -> void:
+	play_anim(last_dir)
+
+func _input(event: InputEvent) -> void:
+	player_movement(event)
 	
-func player_movement(delta: float):
-	var is_moving = true
-	var current_dir = Vector2.ZERO
-	state = States.MOVING
-	if Input.is_action_pressed(&"ui_right"):
+func player_movement(event):
+	var current_dir = Vector2.ZERO	
+	if Input.is_action_just_pressed(&"attack"):
+		if state != States.ATTACKING:
+			previous_state = state
+			state = States.ATTACKING
+	elif Input.is_action_pressed(&"ui_right"):
+		if state == States.IDLE:
+			state = States.MOVING
 		current_dir = Vector2.RIGHT
 	elif Input.is_action_pressed(&"ui_left"):
+		if state == States.IDLE:
+			state = States.MOVING
 		current_dir = Vector2.LEFT
 	elif Input.is_action_pressed(&"ui_up"):
+		if state == States.IDLE:
+			state = States.MOVING
 		current_dir = Vector2.UP
 	elif Input.is_action_pressed(&"ui_down"):
+		if state == States.IDLE:
+			state = States.MOVING
 		current_dir = Vector2.DOWN
 	else:
-		state = States.IDLE
+		if state != States.ATTACKING:
+			state = States.IDLE
 	
-	if current_dir != Vector2.ZERO:
+	if current_dir != Vector2.ZERO && state != States.ATTACKING:
 		last_dir = current_dir
-		
+	
 	velocity = current_dir * SPEED
-	play_anim(last_dir)
-	move_and_slide()
-
+	
 func play_anim(current_dir: Vector2):
 	var anim = $AnimatedSprite2D
 	if state == States.MOVING:
@@ -60,6 +85,10 @@ func play_anim(current_dir: Vector2):
 	elif state == States.IDLE:
 		anim.flip_h = IDLE_ANIMATIONS[last_dir].get("flip", false)
 		anim.play(IDLE_ANIMATIONS[last_dir]["anim"])
+	elif state == States.ATTACKING:
+		anim.flip_h = ATTACK_ANIMATIONS[last_dir].get("flip", false)
+		anim.play(ATTACK_ANIMATIONS[last_dir]["anim"])
+		
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.name == "enemy":
@@ -76,4 +105,9 @@ func _on_enemy_attack(damage: int) -> void:
 		alive = false
 		health = 0 
 		print("YOU HAVE DIED")
-		self.queue_free()		
+		self.queue_free()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print($AnimatedSprite2D.animation)
+	if $AnimatedSprite2D.animation.begins_with("attack_"):
+		state = previous_state
