d7ff6fa07198b9b7f039d0766328f423a9af3db1
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-19 16:47:34 +0100

    Refactor player movement and animation
    Refactor how player movement and animation is handled. Start using a
    basic statemachine and enums to better handle animation and state
    transitions instead of massively nested if statements

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,14 +9,31 @@ var health = 100
 var alive = true
 var attacking = false
 
+const WALK_ANIMATIONS = {
+	Vector2.UP: {"anim": "walk_back"}, 
+	Vector2.DOWN: {"anim": "walk_front"},
+	Vector2.LEFT: {"anim": "walk_side", "flip": true}, 
+	Vector2.RIGHT: {"anim": "walk_side"},
+	Vector2.ZERO: {"anim": "walk_side"}
+	}
+const IDLE_ANIMATIONS = {
+	Vector2.UP: {"anim": "idle_back"},
+	Vector2.DOWN: {"anim": "idle_front"},
+	Vector2.LEFT: {"anim": "idle_side", "flip": true}, 
+	Vector2.RIGHT: {"anim": "idle_side"},
+	Vector2.ZERO: {"anim": "idle_side"}
+	}
+
+enum States {IDLE, MOVING, ATTACKING, DYING}
+var state: States = States.IDLE
+
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
-
 	
 func player_movement(delta: float):
 	var is_moving = true
 	var current_dir = Vector2.ZERO
-	
+	state = States.MOVING
 	if Input.is_action_pressed(&"ui_right"):
 		current_dir = Vector2.RIGHT
 	elif Input.is_action_pressed(&"ui_left"):
@@ -26,34 +43,23 @@ func player_movement(delta: float):
 	elif Input.is_action_pressed(&"ui_down"):
 		current_dir = Vector2.DOWN
 	else:
-		is_moving = false
-		
-	if is_moving:
+		state = States.IDLE
+	
+	if current_dir != Vector2.ZERO:
 		last_dir = current_dir
 		
 	velocity = current_dir * SPEED
-	play_anim(is_moving, last_dir)
+	play_anim(last_dir)
 	move_and_slide()
 
-func play_anim(is_moving: bool, current_dir: Vector2):
+func play_anim(current_dir: Vector2):
 	var anim = $AnimatedSprite2D
-	if current_dir == Vector2.UP:
-		if is_moving:
-			anim.play(&"walk_back")
-		else:
-			if not attacking:
-				anim.play(&"idle_back")
-	elif current_dir == Vector2.DOWN:
-		if is_moving:
-			anim.play(&"walk_front")
-		else:
-			anim.play(&"idle_front")
-	else:
-		anim.flip_h = current_dir == Vector2.LEFT
-		if is_moving:
-			anim.play(&"walk_side")
-		else:
-			anim.play(&"idle_side")
+	if state == States.MOVING:
+		anim.flip_h = WALK_ANIMATIONS[current_dir].get("flip", false)
+		anim.play(WALK_ANIMATIONS[current_dir]["anim"])
+	elif state == States.IDLE:
+		anim.flip_h = IDLE_ANIMATIONS[last_dir].get("flip", false)
+		anim.play(IDLE_ANIMATIONS[last_dir]["anim"])
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.name == "enemy":
